07dd116f4c2aaa334bd33cc97f3f6423877fc542
Mahmoud Farouk <mahmoud.farouk@edafa.com>
2024-09-26 23:50:42 +0300

    added 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,13 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subs, 0 if the given subreddit is invalid."""
     headers = {"User-Agent": "youBot/1.0"}
-    r = requests.get(
+    response = requests.get(
         f"https://www.reddit.com/r/{subreddit}/about.json",
         headers=headers,
         allow_redirects=False,
     )
-    if r.status_code == 200:
-        data = r.json()
+    if response.status_code == 200:
+        data = response.json()
         subs = data["data"]["subscribers"]
         return subs
     return 0
-    
